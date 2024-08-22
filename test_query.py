c74de318470621e00396159d410b6f092984172f
Saber Soleymani <postorder@gmail.com>
2024-08-22 11:32:05 -0400

    debug

--- test_query.py
+++ test_query.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from autogen.agentchat import AssistantAgent
 import chromadb.utils.embedding_functions as embedding_functions
 from autogen.agentchat.contrib.qdrant_retrieve_user_proxy_agent import RetrieveUserProxyAgent
@@ -19,7 +20,6 @@ client = chromadb.PersistentClient(path="./chroma_db")
 collection = client.list_collections()
 print("Available collections:", [c.name for c in collection])
 
-collection_name = "pubmed_10484328"
 # if not client.collection_exists(collection_name=collection_name):
 #     print(f"Collection '{collection_name}' does not exist. Please create the collection first.")
 #     exit()
@@ -32,7 +32,7 @@ llm_config = {
 }
 assistant = AssistantAgent(
     name="assistant",
-    system_message="You are a helpful assistant. You have one job. Job 1. When receiving a message from the user, it is your responsibility to provide an evidence-based answer. Your response must be in JSON format. The JSON response must contain the following keys: 'answer', 'confidence', 'evidence' (cite the source). The 'answer' key must contain the answer to the user's question as 'yes' or 'no'. The 'confidence' key must contain a value between 0 and 1, where 1 indicates the highest confidence. The 'evidence' key must contain a list of evidence supporting the answer. Each piece of evidence must be a dictionary with the following keys: 'text', 'source'. If you don't know the answer, just say `no` as the answer. Don't try to make up an answer.",
+    system_message="You are a helpful assistant. You have one job. Job 1. When receiving a message from the user, it is your responsibility to provide an evidence-based answer. Your response must be in 3 lines. 1: 'answer', 2:'confidence', 3:'evidence'. The 'answer' line contain the answer to the user's question as one word of 'Yes' or 'No'. The 'confidence' line must contain a value between 0 and 1, where 1 indicates the highest confidence. The 'evidence' line must contain a list of evidence supporting the answer from documents including the original text. Each piece of evidence have the following keys: 'text', 'source'. If you don't know the answer, just say `No` as the answer. Don't try to make up an answer if you cannot find the evidence in the provided context.",
     llm_config=llm_config,
 )
 
@@ -51,6 +51,8 @@ assistant = AssistantAgent(
 # Refer to https://microsoft.github.io/autogen/docs/reference/agentchat/contrib/retrieve_user_proxy_agent
 # and https://microsoft.github.io/autogen/docs/reference/agentchat/contrib/vectordb/qdrant
 # for more information on the RetrieveUserProxyAgent and QdrantVectorDB
+DESCRIPTION = 'A biology expert that finds relevant information regarding ion channels in provided texts and responses the queries with `yes` or `no`'
+
 PDF_DIR = "./pdf"
 docs = []
 for pdf_file in os.listdir(PDF_DIR):
@@ -58,32 +60,57 @@ for pdf_file in os.listdir(PDF_DIR):
         pdf_path = os.path.join(PDF_DIR, pdf_file)
         docs.append(pdf_path)
 
-# The ragproxyagent retrieves document chunks based on the embedding similarity, and sends them along with the question to the assistant.
-DESCRIPTION = 'A biology expert that finds relevant information regarding ion channels in provided texts and responses the queries with `yes` or `no`'
-ragproxyagent = RetrieveUserProxyAgent(
-    name="pdfagent",
-    human_input_mode="NEVER",
-    is_termination_msg=None,
-    max_consecutive_auto_reply=5,
-    retrieve_config={ # https://microsoft.github.io/autogen/docs/reference/agentchat/contrib/retrieve_user_proxy_agent/
-        "task": "qa", 
-        "vector_db": "chroma",
-        "description": DESCRIPTION,
-        "client": client, 
-        "docs_path": docs,
-        "collection_name": collection_name,
-        "get_or_create": True,
-        "overwrite": False,
-        "clean_up_tokenization_spaces": True,
-        # "embedding_function": openai_ef,
-        # "embedding_model": OPENAI_EMBEDDING_MODEL,
-        "model": MODEL_NAME,
-        }
-)
-ragproxyagent.description = DESCRIPTION
+# iterate over data.csv
+df = pd.read_csv('human_IC_annotation_sample.csv')
+
+for index, row in df.iterrows():
+    uniprot_id = row["Uniprot"].strip()
+    pubmed_id = row["PubMed"].strip()
+    collection_name = "pubmed_" + pubmed_id
+    print(f"Processing Uniprot Id {uniprot_id} in '{collection_name}'...")
+    #
+    # collection_name = "pubmed_10484328"
+    # The ragproxyagent retrieves document chunks based on the embedding similarity, and sends them along with the question to the assistant.
+    ragproxyagent = RetrieveUserProxyAgent(
+        name="pdfagent",
+        human_input_mode="NEVER",
+        is_termination_msg=None,
+        max_consecutive_auto_reply=5,
+        retrieve_config={ # https://microsoft.github.io/autogen/docs/reference/agentchat/contrib/retrieve_user_proxy_agent/
+            "task": "qa", 
+            "vector_db": "chroma",
+            "description": DESCRIPTION,
+            "client": client, 
+            "docs_path": docs,
+            "collection_name": collection_name,
+            "get_or_create": True,
+            "overwrite": False,
+            "clean_up_tokenization_spaces": True,
+            # "embedding_function": openai_ef,
+            # "embedding_model": OPENAI_EMBEDDING_MODEL,
+            "model": MODEL_NAME,
+            }
+    )
+    ragproxyagent.description = DESCRIPTION
 
 
-assistant.reset() # to forget previous conversations
-query = "Is there any evidence that pottasium is the ion selectivity of the K_V ion channel?"
-chat_results = ragproxyagent.initiate_chat(assistant, message=ragproxyagent.message_generator, problem=query)
-print(chat_results.summary)
+    assistant.reset() # to forget previous conversations
+    ion = row["Ion"].stript()
+    family = row["Family"].strip()
+    gate_mechanism = row["GateMechanism"].strip()
+    query1 = f"Is there any evidence that `{ion}` is the ion selectivity of the `{family}` ion channel?"
+    chat_results1 = ragproxyagent.initiate_chat(assistant, message=ragproxyagent.message_generator, problem=query1)
+    
+    query = " voltage-gated mechanism for the K_V ion channel?"
+    query2 = f"Does this article provide evidence for `{gate_mechanism}` as the gate mechanism for the `{family}` ion channel?"
+    chat_results2 = ragproxyagent.initiate_chat(assistant, message=ragproxyagent.message_generator, problem=query2)
+    #print(chat_results.summary)
+    with open('results.txt', 'a') as f:
+        f.write(f"UniProt: {uniprot_id}\n")
+        f.write(f"PubMed: {pubmed_id}\n")
+        f.write(f"Query 1: {query1}\n")
+        f.write(f"Results 1: {chat_results1.summary}\n")
+        f.write(f"Query 2: {query2}\n")
+        f.write(f"Results 1: {chat_results2.summary}\n")
+        f.write("\n")
+        f.write("---------------------------------------------\n")
